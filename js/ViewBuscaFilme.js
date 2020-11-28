007c16f6d83ab787e46e6817c12eeedcace0dd60
Scarlate <scarlate100@gmail.com>
2020-11-28 08:01:16 -0300

    barra de pesquisa arrumada

--- js/ViewBuscaFilme.js
+++ js/ViewBuscaFilme.js
@@ -1,5 +1,16 @@
 class ViewBuscaFilme {
+    constructor() {
+        this.mostra = document.getElementById('navbarToggleExternalContent')
+    }
     pequisaFilme(result) {
+
+        //cria tag da div dos filmes
+        this.mostra.innerHTML = `<div class=" p-1">
+        <h5 class="letter-color" id="titulo"></h5>
+        <span class="letter-color" id="ano"></span>
+    </div>`
+
+        //pegando elemento da API e mostrando na tag criada
         let img = document.getElementById('ano')
         let titulo = document.getElementById('titulo')
         let ano = document.getElementById('ano')
